ae36797ed4018d25f05fe4ace922999644005ce5
Katerina Frolova <ekaterina.frolova40@gmail.com>
2019-03-22 17:04:41 +0300

    How generators work

--- 3_generators.py
+++ 3_generators.py
@@ -1,19 +1,15 @@
 from time import time
 
-
-
 def gen(s):
     for i in s:
         yield i
 
-g = gen('kate')
-
+n = gen('kate')
 
 
 def get_filename():
     i = 1
     while True:
-
         pattern = "file-{}.jpeg"
         t = int(time() * 1000)
 
@@ -22,5 +18,24 @@ def get_filename():
 
         i += 1
 
+def gen(s):
+    for i in s:
+        yield i
 
-g = get_filename()
+def gen2(n):
+    for i in range(n):
+        yield i
+
+g1 = gen("kate")
+g2 = gen2(4)
+
+tasks = [g1, g2]
+
+while tasks:
+    task = tasks.pop(0)
+    try:
+        i = next(task)
+        print(i)
+        tasks.append(task)
+    except StopIteration:
+        pass
